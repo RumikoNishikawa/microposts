e33239c45ec479a04c5e0b667d023c6c1ab6a723
RumikoNishikawa <momonga919@gmail.com>
2016-03-05 22:45:36 +0000

    create writting form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'login', to:'sessions#create'
   delete 'logout', to:'sessions#destroy'
   
-  resources :microposts
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts
 end
