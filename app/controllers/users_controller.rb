ce76a39c3d90ad932a9f1ac355678a49c544f06e
RumikoNishikawa <momonga919@gmail.com>
2016-03-05 13:52:06 +0000

    create edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :collect_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -9,19 +11,40 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "Welcome to Rumiko's App! Please Enjoy!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+      
+  def edit
+    @user = User.find(params[:id])
+  end
   
-      if @user.save
-        flash[:success] = "Welcome to Rumiko's App! Please Enjoy!"
-        redirect_to @user
-       
-      else
-       render 'new'
-      end
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = "Welcome to Rumiko's App! Please Enjoy!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   private
+ 
+  def collect_user
+    user = User.find(params[:id])
+    redirect_to(root_url) if user != current_user
+  end 
   
   def user_params
-    params.require(:user).permit(:name, :email, :password,:password_comfirmation)
+    params.require(:user).permit(:name, :email, :location, :introduction, :password,:password_comfirmation)
   end
+  
+  
 end
